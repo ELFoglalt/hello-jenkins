fd5395501916124763950df4fa1b1bb9f1759db1
Máté Kerekes <everydaymadnes@gmail.com>
2019-02-07 02:48:45 +0100

    Added .prettierrc.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //importing node framework
 var express = require("express");
- 
+
 var app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
-app.get("/", function (req, res) {
- res.send("Wello world!");
+app.get("/", function(req, res) {
+    res.send(
+        "Wello world! Now with automatic deployment, thanks to Jenkins CI!",
+    );
 });
 
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
- 
+
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -2,9 +2,13 @@ var request = require("supertest");
 var app = require("../index.js");
 
 describe("GET /", function() {
- it("respond with hello world", function(done) {
-
- //navigate to root and check the the response is "hello world"
- request(app).get("/").expect("Wello world!", done);
- });
+    it("respond with hello world", function(done) {
+        //navigate to root and check the the response is "hello world"
+        request(app)
+            .get("/")
+            .expect(
+                "Wello world! Now with automatic deployment, thanks to Jenkins CI!",
+                done,
+            );
+    });
 });
